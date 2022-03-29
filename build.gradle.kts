d8a875553ccddee8c69619e8796b89cc7f3bf132
Ragunath Jawahar <ragunath@redgreen.io>
2022-03-29 21:37:37 +0530

    build: add Moshi

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,8 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-  kotlin("jvm") version "1.6.10"
+  kotlin("jvm") version "1.4.10"
+  kotlin("kapt") version "1.4.10"
   application
 }
 
@@ -12,9 +13,18 @@ repositories {
   mavenCentral()
 }
 
+val moshiVersion = "1.11.0"
+
 dependencies {
-  testImplementation(kotlin("test"))
+  implementation("com.squareup.moshi:moshi:$moshiVersion")
+  kapt("com.squareup.moshi:moshi-kotlin-codegen:$moshiVersion")
+
+  testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.0")
+  testImplementation("org.junit.jupiter:junit-jupiter-params:5.7.0")
+  testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.0")
+
   testImplementation("com.google.truth:truth:1.1.3")
+
   testImplementation("com.approvaltests:approvaltests:15.0.0")
 }
 
